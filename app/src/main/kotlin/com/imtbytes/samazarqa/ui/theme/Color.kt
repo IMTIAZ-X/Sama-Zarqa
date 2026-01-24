0446eff5fa5bcff6cde86679fbeda1a4059ac029
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2026-01-24 23:32:42 +0600

    Add Color.kt for theme color definitions

---
+++ app/src/main/kotlin/com/imtbytes/samazarqa/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.imtbytes.samazarqa.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF3D89FF)
+
+val PrimaryBlueDark = Color(0xFF005BCB)
+val PrimaryBlueLight = Color(0xFF75AFFF)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val BackgroundLight = Color(0xFFFBFDFF)
+val SurfaceLight = Color(0xFFFFFFFF)
