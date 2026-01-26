d648bbdb273ec103c06b7cb2f6bd31842a9ab03f
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2026-01-26 10:14:21 +0600

    Add files via upload

--- app/src/main/kotlin/com/imtbytes/samazarqa/screens/SplashScreen.kt
+++ app/src/main/kotlin/com/imtbytes/samazarqa/screens/SplashScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.imtbytes.samazarqa.ui.theme.SamazarqaTheme
+import com.imtbytes.samazarqa.ui.theme.*
 
 @Composable
 fun SplashScreen() {
