1fd9d037686963ea064363bd1c4aa1abfb918a23
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2026-02-13 00:29:13 +0600

    Add files via upload

--- app/src/main/kotlin/com/imtbytes/samazarqa/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/imtbytes/samazarqa/screens/HomeScreen.kt
@@ -103,7 +103,7 @@ fun HomeScreen(
                         NavItem.QR -> ScannerScreen(isDarkTheme)
                         NavItem.Downloader -> DownloaderScreen(isDarkTheme)
                         NavItem.Profile -> ProfileScreen(isDarkTheme)
-                        NavItem.Tool -> ToolScreen(isDarkTheme)
+                        NavItem.Tool -> ToolScreen()
                         NavItem.Settings -> SettingScreen(currentTheme, onThemeChanged)
                     }
                 }
