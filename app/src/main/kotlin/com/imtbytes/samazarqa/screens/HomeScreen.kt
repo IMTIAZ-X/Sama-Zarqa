ef35ca7a776c4178485d9e9913b6e96ed449cf65
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2026-02-12 18:45:45 +0600

    Add files via upload

--- app/src/main/kotlin/com/imtbytes/samazarqa/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/imtbytes/samazarqa/screens/HomeScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.QrCodeScanner
 import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material.icons.filled.build
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -45,7 +45,7 @@ enum class NavItem(val icon: ImageVector, val label: String) {
     QR(Icons.Default.QrCodeScanner, "QR"),
     Downloader(Icons.Default.Download, "Download"),
     Profile(Icons.Default.Person, "Profile"),
-    Type(Icons.Default.build, "Type"),
+    Type(Icons.Default.Build, "Type"),
     Settings(Icons.Default.Settings, "Setting")
 }
 
